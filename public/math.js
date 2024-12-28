d0a1f517499839a2fc34418170feaf0e8d8b557c
horiz <horiz@Horizon.com>
2024-12-28 10:57:40 -0500

    Add more lerping to space of price values.

--- public/math.js
+++ public/math.js
@@ -4,6 +4,14 @@ math.lerp = (a, b, t) => {
     return a + (b - a) * t;
 }
 
+math.invLerp = (a, b, v) => {
+    return (v -a) / (b - a);
+}
+
+math.remap = (oldA, oldB, newA, newB, v) => {
+    return math.lerp(newA, newB, math.invLerp(oldA, oldB, v));
+}
+
 math.formatNumber = (num, numDec=0) => {
     return num.toFixed(numDec);
 }
