151003899faddc2f030479c5ab9c9efb73ef647c
horiz <horiz@Horizon.com>
2024-12-28 11:34:05 -0500

    Set chart bounds for pixel and data.

--- public/chart.js
+++ public/chart.js
@@ -15,5 +15,42 @@ class Chart {
 
         this.margin = options.size*0.1;
         this.transparency = 0.5;
+
+        this.pixelBounds = this.#getPixelBounds();
+        this.dataBounds = this.#getDataBounds();
+
+        this.#draw();
+    }
+
+    #getPixelBounds() {
+        const {canvas, margin} = this;
+        const bounds = {
+            left: margin,
+            right: canvas.width - margin,
+            top: margin,
+            bottom: canvas.height - margin
+        };
+        return bounds;
+    }
+
+    #getDataBounds() {
+        const {samples} = this;
+        const x = samples.map(s => s.point[0]);
+        const y = samples.map(s => s.point[1]);
+        const minX = Math.min(...x);
+        const maxX = Math.max(...x);
+        const minY = Math.min(...y);
+        const maxY = Math.max(...y);
+        const bounds = {
+            left: minX,
+            right: maxX,
+            top: maxY,
+            bottom: minY
+        };
+        return bounds;
+    }
+
+    #draw() {
+
     }
 }
